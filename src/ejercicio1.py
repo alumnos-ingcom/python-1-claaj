32f6257f684e663b6514af2ee548a65f74b3d525
Matías Cajal <matias.cajal@hotmail.com>
2022-06-13 22:41:18 -0300

    Agregado comentario y emprolijado de codigo

--- src/ejercicio1.py
+++ src/ejercicio1.py
@@ -10,35 +10,31 @@ Se quiere transformar temperaturas en grados fahrenheit a grados centígrados y 
 Escribir las funciones para convertir la temperatura en grados centigrados en fahrenheit como un número decimal,
 utilice esta formula para calcular los grados centígrados y retorne el resultado obtenido. Y viceversa.
 """
-# Reemplazar por las funciones del ejercicio
 
-def convertir_a_fahrrenheit(centigrados):
+def convertir_a_fahrrenheit(centigrados): #función para convertir de °C a °F
     return (centigrados * 9/5) + 32
 
-def convertir_a_centigrados(fahrenheit):
+def convertir_a_centigrados(fahrenheit): #función para convertir de °F a °C
     return (fahrenheit - 32) / (9/5)
 
-def principal():  # pragma: no cover
+def principal():
     """
     Esta función es la que se encarga de la parte 'interactiva' del ejercicio
     (La entrada, la llamada al algoritmo y la salida)
     """
+    # Se pide por la opción
     opcion = int(input('Convertir a:\n 1) Centigrados\n 2) Fahrenheit\n Ingrese la opción elegida: '))
 
-    if opcion == 1:
+    if opcion == 1: # Si es 1 se pide el valor en centigrados e imprime la conversión
         valor = float(input('Ingrese valor en centigrados: '))
         print(f'{valor} °C equivale a {convertir_a_fahrrenheit(valor)} °F')
 
-    elif opcion == 2:
+    elif opcion == 2: #Si es 2 se pide el valor en fahrenheit e imprime  la conversión
         valor = float(input('Ingrese valor en fahrenheit: '))
         print(f'{valor} °F equivale a {convertir_a_centigrados(valor)} °C')
 
-    else:
+    else: # En caso de no 1 o 2 se imprime el error
         print('Opción inválida, corra de nuevo el programa e intente nuevamente.')
 
-
-    pass
-
-
-if __name__ == "__main__":  # pragma: no cover
+if __name__ == "__main__":
     principal()

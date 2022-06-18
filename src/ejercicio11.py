7263df2f4b959cf52ecd347712d1f78e609b26c4
Matías Cajal <matias.cajal@hotmail.com>
2022-06-17 22:27:14 -0300

    Agregado de ejercicio11 y su test

---
+++ src/ejercicio11.py
@@ -0,0 +1,31 @@
+################
+# Matías Cajal - @claaj
+# UNRN Andina - Introducción a la Ingeniería en Computación
+################
+
+"""
+11. Multiplos de
+
+Escribir una función que indique con True si un número entero es multiplo de otro, utilizando sumas y restas.
+"""
+
+def es_multiplo(num, mul):
+    """
+    Devuelve si un número es multiplo de otro
+    """
+    aux = 0
+    while aux < num:
+        aux += mul
+    return True if aux == num else False
+
+def principal():
+    """
+    Esta función es la que se encarga de la parte 'interactiva' del ejercicio
+    (La entrada, la llamada al algoritmo y la salida)
+    """
+    numero = int(input('Ingrese un número: '))
+    multiplo = int(input('Ingrese el múltiplo: '))
+    print(f'{es_multiplo(numero, multiplo)}')
+
+if __name__ == "__main__":
+    principal()
